00f81e621196a95656f8448843691e32c5f2b6a5
smach <smachlis@gmail.com>
2020-12-02 22:27:12 -0500

    Added county data frame

--- R/data_prep.R
+++ R/data_prep.R
@@ -2,20 +2,30 @@ library(dplyr)
 library(rio)
 library(readr)
 
+options(scipen = 999)
 county_list <- rio::import("data/MACountiesAndPlaces.xlsx") %>%
   mutate(
     Place = gsub("*", "", Place, fixed = TRUE),
-    Place = tolower(Place)
+    Place = tolower(Place),
+    County = gsub("((Dukes)|(Nantucket))", "Dukes and Nantucket", County)
   )
 county_lookup <- county_list$County
 names(county_lookup) <- trimws(county_list$Place)
 
-mydata <- rio::import("data/weekly-dashboard-data-11-27-2020.xlsx", sheet = "City_town") %>%
+places <- rio::import("data/weekly-dashboard-data-11-27-2020.xlsx", sheet = "City_town") %>%
   mutate(
     Place = trimws(gsub("*", "", `City/Town`, fixed = TRUE)),
     County = county_lookup[tolower(Place)],
-    Cases = readr::parse_integer(`Total Case Counts`),
+    Cases = readr::parse_integer(`Two Week Case Counts`),
     Positivity = readr::parse_number(`Percent Positivity`)
   ) %>%
   filter(!is.na(County) & !is.na(Cases)) %>%
   select(Place, County, Cases, Positivity)
+
+counties <- rio::import("data/weekly-dashboard-data-11-27-2020.xlsx", sheet = "county") %>%
+  mutate(
+    County = trimws(gsub(" Count[yi]e?s?", "", County)),
+    Positivity = round(readr::parse_number(`Percent Positivity (Last 14 days)`), 3)
+  ) %>%
+  filter(!is.na(County) & !is.na(Positivity & County != "State")) %>%
+  select(County, Cases = `Case Count (Last 14 Days)`, Positivity)
