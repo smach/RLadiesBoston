db8b7d2c10ff86d5cc758c3fa25871334c53ab51
smach <smachlis@gmail.com>
2020-12-27 12:20:48 -0500

    Added expandable-row tables

--- data/get_latest_covid_act_now_data.R
+++ data/get_latest_covid_act_now_data.R
@@ -18,4 +18,4 @@ download.file(my_url2, "data/all_covid_by_state.csv")
 
 # Get Census regions
 download.file("https://raw.githubusercontent.com/cphalpert/census-regions/master/us%20census%20bureau%20regions%20and%20divisions.csv", destfile = "data/census_regions.csv")
-
+my_js_
